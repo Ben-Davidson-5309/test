2b35d5dbb7093d862c2dc19d4e2dd282db05e2c5
Benjamin Davidsone <bendavidson5309@icloud.comm>
2025-04-13 12:37:33 -0700

    saving my npms

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", req, res) => {
+    res.render("indez");
+
+}
+
+app.get("/about", (req, res) => {
+    res.send("About this page");
+}); 
+
+app.listen(PORT, () => {
+    debug(`Listening on port ${chalk.green(PORT)}`);
+});
