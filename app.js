136b162f2f281dca572ec1fc57971f047df608ce
Benjamin Davidsone <bendavidson5309@icloud.comm>
2025-04-17 11:25:30 -0700

    I use ejs for name and in the app.js I added the app.post for the ejs input/output

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const bodyParser = require("body-parser"); 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
@@ -20,6 +24,17 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.send("About this page");
 }); 
+
+app.get("/ejsTest", (req, res) => {
+    res.render("ejsTest-Input");
+});
+
+app.post("/ejsTest", (req, res) => {
+    res.render("testName-output", {
+        firstName: req.body.fname, 
+        lastName: req.body.lname,
+    });
+});
 
 app.listen(PORT, () => {
     debug(`Listening on port ${chalk.green(PORT)}`);
